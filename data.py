619b7c519d6e54eb344aa7fb27627ca300c606fc
DESKTOP-IG5VE3F\asus <rifqihadiputra@gmail.com>
2023-05-07 00:51:10 +0700

    Tugas Selenium Rifqi

---
+++ data.py
@@ -0,0 +1,25 @@
+class register ():
+    firstName = "Rifqi"
+    lastName = "Hadi"
+    email = "rifqirandom2@gmail.com"
+    password = "test123"
+    confirmPassword = "test123"
+    successRegister = "Your registration completed"
+    validationFirstName = "First name is required."
+
+    
+class login ():
+    validEmail = "rifqirandom2@gmail.com"
+    validPassword = "test123"
+    invalidEmail = "rifqi.invalid@gmail.com"
+    invalidPassword = "invalidpassword"
+    errorMessage = "Login was unsuccessful. Please correct the errors and try again."
+    
+class address ():
+    city = "Calgary"
+    address1 = "141 East River Lane Montague, PE C0A 6P3"
+    postalCode = "K0A 0A2"
+    phoneNumber = "+12505550199"
+    successOrder = "Your order has been successfully processed!"
+    
+    
